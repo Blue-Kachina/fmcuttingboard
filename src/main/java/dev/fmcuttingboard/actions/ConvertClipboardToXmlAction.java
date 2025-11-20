e17085e0bfca9df78dba26f05482c57b4190ea52
Matthew Leering <matthew.leering@directimpactsolutions.com>
2025-11-20 08:34:32 -0500

    Register Convert FileMaker Clipboard To XML action under the submenu

---
+++ src/main/java/dev/fmcuttingboard/actions/ConvertClipboardToXmlAction.java
@@ -0,0 +1,16 @@
+package dev.fmcuttingboard.actions;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Phase 1.2: Action placeholder registered under Tools > FMCuttingBoard.
+ * Actual behavior will be implemented in later phases per roadmap.
+ */
+public class ConvertClipboardToXmlAction extends AnAction {
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        // Placeholder: functionality will be implemented in Phase 3 per roadmap.
+    }
+}
