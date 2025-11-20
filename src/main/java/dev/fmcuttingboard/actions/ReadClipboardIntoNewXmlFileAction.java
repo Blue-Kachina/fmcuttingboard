816c7157000f8078d26bb8eb25201c9d6fbff9a7
Matthew Leering <matthew.leering@directimpactsolutions.com>
2025-11-20 08:35:57 -0500

    Register Read Clipboard Into New XML File action under the submenu

---
+++ src/main/java/dev/fmcuttingboard/actions/ReadClipboardIntoNewXmlFileAction.java
@@ -0,0 +1,15 @@
+package dev.fmcuttingboard.actions;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Phase 1.2: Placeholder for creating a new XML file from clipboard contents.
+ */
+public class ReadClipboardIntoNewXmlFileAction extends AnAction {
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        // Placeholder: actual implementation will arrive in Phase 4.
+    }
+}
