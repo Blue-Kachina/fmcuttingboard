0a42d05ee6a94fc5b9032523ef328c79961b7e00
Matthew Leering <matthew.leering@directimpactsolutions.com>
2025-11-21 08:34:10 -0500

    Oops.  Forgot to add a file to my commits

---
+++ src/test/java/dev/fmcuttingboard/fm/FmXmlParserTest.java
@@ -0,0 +1,50 @@
+package dev.fmcuttingboard.fm;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FmXmlParserTest {
+
+    private final FmXmlParser parser = new FmXmlParser();
+
+    @Test
+    void parsesFieldsSnippet() throws Exception {
+        String xml = """
+                <fmxmlsnippet version="1">
+                  <FieldDefinition name="CustomerID"/>
+                </fmxmlsnippet>
+                """;
+        ParsedSnippet sn = parser.parse(xml);
+        assertNotNull(sn);
+        assertEquals("1", sn.getVersion());
+        assertTrue(sn.getElementTypes().contains(ElementType.FIELDS));
+        assertTrue(sn.getFieldNames().contains("CustomerID"));
+    }
+
+    @Test
+    void parsesScriptSnippet() throws Exception {
+        String xml = """
+                <fmxmlsnippet>
+                  <Script name="DoWork">
+                    <Step id="1"/>
+                  </Script>
+                </fmxmlsnippet>
+                """;
+        ParsedSnippet sn = parser.parse(xml);
+        assertTrue(sn.getElementTypes().contains(ElementType.SCRIPTS));
+        assertTrue(sn.getScriptNames().contains("DoWork"));
+    }
+
+    @Test
+    void rejectsNonSnippetRoot() {
+        String xml = "<root><child/></root>";
+        assertThrows(ConversionException.class, () -> parser.parse(xml));
+    }
+
+    @Test
+    void rejectsEmptySnippetContent() {
+        String xml = "<fmxmlsnippet/>";
+        assertThrows(ConversionException.class, () -> parser.parse(xml));
+    }
+}
