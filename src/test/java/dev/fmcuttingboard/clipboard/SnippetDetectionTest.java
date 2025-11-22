584c8d0d8b0f5110f3525701bd25fb45148f6f63
Matthew Leering <matthew.leering@directimpactsolutions.com>
2025-11-22 09:46:42 -0500

    Add unit tests for each snippet type detection

---
+++ src/test/java/dev/fmcuttingboard/clipboard/SnippetDetectionTest.java
@@ -0,0 +1,50 @@
+package dev.fmcuttingboard.clipboard;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SnippetDetectionTest {
+
+    @Test
+    void detectsScriptSteps() {
+        String xml = "<fmxmlsnippet type=\"FMObjectList\"><Step id=\"1\"/></fmxmlsnippet>";
+        assertEquals(DefaultClipboardService.SnippetType.SCRIPT_STEPS,
+                DefaultClipboardService.detectSnippetType(xml));
+    }
+
+    @Test
+    void detectsFieldDefinition() {
+        String xml = "<fmxmlsnippet type=\"FMObjectList\"><FieldDefinition name=\"X\"/></fmxmlsnippet>";
+        assertEquals(DefaultClipboardService.SnippetType.FIELD_DEFINITION,
+                DefaultClipboardService.detectSnippetType(xml));
+    }
+
+    @Test
+    void detectsFieldTagVariant() {
+        String xml = "<fmxmlsnippet type=\"FMObjectList\"><Field name=\"Y\"/></fmxmlsnippet>";
+        assertEquals(DefaultClipboardService.SnippetType.FIELD_DEFINITION,
+                DefaultClipboardService.detectSnippetType(xml));
+    }
+
+    @Test
+    void detectsTableDefinition() {
+        String xml = "<fmxmlsnippet type=\"FMObjectList\"><BaseTable name=\"T\"/></fmxmlsnippet>";
+        assertEquals(DefaultClipboardService.SnippetType.TABLE_DEFINITION,
+                DefaultClipboardService.detectSnippetType(xml));
+    }
+
+    @Test
+    void detectsLayoutObjects() {
+        String xml = "<fmxmlsnippet type=\"FMObjectList\"><Layout name=\"L\"/><ObjectList/></fmxmlsnippet>";
+        assertEquals(DefaultClipboardService.SnippetType.LAYOUT_OBJECTS,
+                DefaultClipboardService.detectSnippetType(xml));
+    }
+
+    @Test
+    void unknownWhenNoHeuristicsMatch() {
+        String xml = "<fmxmlsnippet type=\"FMObjectList\"><UnknownTag/></fmxmlsnippet>";
+        assertEquals(DefaultClipboardService.SnippetType.UNKNOWN,
+                DefaultClipboardService.detectSnippetType(xml));
+    }
+}
